528d9977ab176e3b9d544fa4dfa1d30fd73faf0e
JJIMENEZ <jjimenez@3tech-panama.com>
2024-01-22 16:56:07 -0500

    cambios ront

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  base:'http://localhost:3001/#/student/',
+  base:'http://erp.organo-judicial.com/#/student/',
   production: false,
   apiUrlTeacher:'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/Teacher/',
   apiUrl:'https://isjupauthenticationservice.azurewebsites.net/api/v1/account/',
