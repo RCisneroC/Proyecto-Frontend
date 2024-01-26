de66e5f8e46378eef384042d15b0efff995e313e
JJIMENEZ <jjimenez@3tech-panama.com>
2024-01-26 17:38:24 -0500

    cambios

--- src/app/teaching-management/teaching-admission-external/teaching-admission-external.component.ts
+++ src/app/teaching-management/teaching-admission-external/teaching-admission-external.component.ts
@@ -17,8 +17,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./teaching-admission-external.component.scss']
 })
 export class TeachingAdmissionExternalComponent extends UnsubscribeOnDestroyAdapter
-implements OnInit{
-  
+  implements OnInit {
+
   teacherForm!: UntypedFormGroup;
   documentForm!: UntypedFormGroup;
   processList = [
@@ -33,7 +33,7 @@ implements OnInit{
     'year',
     'actions'
   ];
-  
+
   displayedColumnsTraining = [
     'trainingId',
     'institution',
@@ -43,15 +43,15 @@ implements OnInit{
     'degreeObtained',
     'actions'
   ];
-  
 
-  
+
+
   displayedColumnsSpecialty = [
     'specialtyId',
     'name',
     'actions'
   ];
-  
+
   displayedColumnsExperience = [
     'experienceId',
     'description',
@@ -60,17 +60,17 @@ implements OnInit{
     'endDate',
     'actions'
   ];
-  
 
-DataTeacher!:Teacher;
-DataExperience:Experience[]= [];
-DataTraining:Training[]= [];
-fechaActual!: string;
-fechaA: string | undefined;
-header!: string;
-experience?: Experience;
-training?: Training;
-_Form_Data = new FormData();
+
+  DataTeacher!: Teacher;
+  DataExperience: Experience[] = [];
+  DataTraining: Training[] = [];
+  fechaActual!: string;
+  fechaA: string | undefined;
+  header!: string;
+  experience?: Experience;
+  training?: Training;
+  _Form_Data = new FormData();
 
   FormsEFDocument!: UntypedFormGroup;
 
@@ -78,44 +78,44 @@ _Form_Data = new FormData();
 
 
 
-constructor( private activatedRoute: ActivatedRoute,
-public _ActivityService: ActivityDetailService,
-public _teacherService: TeacherService,
-public _dialog: MatDialog,
-private _nav:Router,
-private fb: UntypedFormBuilder,
-){
-  super();
-  
-}
+  constructor(private activatedRoute: ActivatedRoute,
+    public _ActivityService: ActivityDetailService,
+    public _teacherService: TeacherService,
+    public _dialog: MatDialog,
+    private _nav: Router,
+    private fb: UntypedFormBuilder,
+  ) {
+    super();
+
+  }
 
 
   ngOnInit() {
-    this.DataTeacher=new Teacher();
+    this.DataTeacher = new Teacher();
     const fechaActual = new Date();
-    this.fechaA=fechaActual.toLocaleDateString('es-PA');
+    this.fechaA = fechaActual.toLocaleDateString('es-PA');
     this.teacherForm = this.createTeacherForm();
-  
-    this.FormsEFDocument= this.fb.group({
-      Photo:new FormControl([]),
-      CIP:new FormControl([]),
-      Title:new FormControl([]),
-      CV:new FormControl([]),
-      
+
+    this.FormsEFDocument = this.fb.group({
+      Photo: new FormControl([]),
+      CIP: new FormControl([]),
+      Title: new FormControl([]),
+      CV: new FormControl([]),
+
     });
-   
-   
-    this.teacherForm=this.fb.group({
-      teacherId :new FormControl(0),
-      cedula:['',[Validators.required]],
-      name:['',[Validators.required]],
-      lastName:['',[Validators.required]],
-      placeOfBirth:['',[Validators.required]],
-      dateOfBirth:['',[Validators.required]],
-      placeResidence:['',[Validators.required]],
-      email:['',[Validators.required]],
-      telephoneNumber:['',[Validators.required]],
-      carreraId:['',[Validators.required]],
+
+
+    this.teacherForm = this.fb.group({
+      teacherId: new FormControl(0),
+      cedula: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      placeOfBirth: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      placeResidence: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      telephoneNumber: ['', [Validators.required]],
+      carreraId: [''],
       selected: new FormControl(false),
       listCourse: new FormControl([]),
       listTraining: new FormControl([]),
@@ -126,19 +126,19 @@ private fb: UntypedFormBuilder,
       createdBy: new FormControl("")
     })
   }
-  
+
   cedulaExist() {
     return (control: FormControl) => {
       const cedula = control.value;
-  
+
       if (!cedula) {
         return null; // Don't validate empty emails
       }
-  
+
       return this._teacherService.getExisteCedula(cedula)
     };
   }
-  AddExperience(){
+  AddExperience() {
     const dialogRef = this._dialog.open(AddExperienceComponent, {
       data: {
         experience: this.experience,
@@ -146,30 +146,30 @@ private fb: UntypedFormBuilder,
       },
       disableClose: true,
     });
-    dialogRef.afterClosed().subscribe((result:Experience) => {
+    dialogRef.afterClosed().subscribe((result: Experience) => {
       if (result == undefined) {
         return;
-        }
-        this.DataExperience=[];
-        
-        if(this.DataTeacher.listExperience.length>0){
-          const IdMayor = this.DataTeacher.listExperience.reduce((previous, current) => {
-            return current.experienceId > previous.experienceId ? current : previous;
-          });
-          result.experienceId=IdMayor.experienceId+1;
-        }else{
-          result.experienceId=1;
-        }
-      
-        this.DataExperience.push(result);
-        
-        this.DataTeacher.listExperience=[...this.DataTeacher.listExperience, ...this.DataExperience]
-        
+      }
+      this.DataExperience = [];
+
+      if (this.DataTeacher.listExperience.length > 0) {
+        const IdMayor = this.DataTeacher.listExperience.reduce((previous, current) => {
+          return current.experienceId > previous.experienceId ? current : previous;
+        });
+        result.experienceId = IdMayor.experienceId + 1;
+      } else {
+        result.experienceId = 1;
+      }
+
+      this.DataExperience.push(result);
+
+      this.DataTeacher.listExperience = [...this.DataTeacher.listExperience, ...this.DataExperience]
+
     });
   }
-  
-  
-  AddTraining(){
+
+
+  AddTraining() {
     const dialogRef = this._dialog.open(AddTrainingComponent, {
       data: {
         training: this.training,
@@ -177,131 +177,128 @@ private fb: UntypedFormBuilder,
       },
       disableClose: true,
     });
-    dialogRef.afterClosed().subscribe((result:Training) => {
+    dialogRef.afterClosed().subscribe((result: Training) => {
       if (result == undefined) {
         return;
-        }
-        this.DataTraining=[];
-        
-        if(this.DataTeacher.listTraining.length>0){
-          const IdMayor = this.DataTeacher.listTraining.reduce((previous, current) => {
-            return current.trainingId > previous.trainingId ? current : previous;
-          });
-          result.trainingId=IdMayor.trainingId+1;
-        }else{
-          result.trainingId=1;
-        }
-      
-        this.DataTraining.push(result);
-        
-        this.DataTeacher.listTraining=[...this.DataTeacher.listTraining, ...this.DataTraining]
-        
+      }
+      this.DataTraining = [];
+
+      if (this.DataTeacher.listTraining.length > 0) {
+        const IdMayor = this.DataTeacher.listTraining.reduce((previous, current) => {
+          return current.trainingId > previous.trainingId ? current : previous;
+        });
+        result.trainingId = IdMayor.trainingId + 1;
+      } else {
+        result.trainingId = 1;
+      }
+
+      this.DataTraining.push(result);
+
+      this.DataTeacher.listTraining = [...this.DataTeacher.listTraining, ...this.DataTraining]
+
     });
   }
-  RemoveExperience(row:Experience){
- 
-        this.DataExperience=[];
-        this.DataExperience=this.DataTeacher.listExperience.filter(x=>x.experienceId!=row.experienceId)
-        this.DataTeacher.listExperience=[...this.DataExperience]
-        
+  RemoveExperience(row: Experience) {
+
+    this.DataExperience = [];
+    this.DataExperience = this.DataTeacher.listExperience.filter(x => x.experienceId != row.experienceId)
+    this.DataTeacher.listExperience = [...this.DataExperience]
+
   }
-  Regresar(){
+  Regresar() {
     this._nav.navigate(['/teaching-management/teacher-list/']);
   }
-  
-//   this.FormsEFDocument=this.fb.group({
-//     Photo:[''],
-//     CIP:[''],
-//     Title:[''],
-//     Credits:[''],
-//     Idoneidad:[''],
-//     LetterMotivation:[''],
-//     LetterAval:[''],
-//  })
-  
-  createTeacherForm(): UntypedFormGroup{
+
+  //   this.FormsEFDocument=this.fb.group({
+  //     Photo:[''],
+  //     CIP:[''],
+  //     Title:[''],
+  //     Credits:[''],
+  //     Idoneidad:[''],
+  //     LetterMotivation:[''],
+  //     LetterAval:[''],
+  //  })
+
+  createTeacherForm(): UntypedFormGroup {
     return this.fb.group({
       teacherId: new FormControl(0),
-      cedula: [this.DataTeacher.cedula,[Validators.required]],
+      cedula: [this.DataTeacher.cedula, [Validators.required]],
       name: new FormControl(this.DataTeacher?.name, [Validators.required]),
       lastName: new FormControl(this.DataTeacher?.lastName, [Validators.required]),
-      email: new FormControl(this.DataTeacher?.email, [Validators.required,Validators.email]),
-      selected: new FormControl(this.DataTeacher?.selected),
+      email: new FormControl(this.DataTeacher?.email, [Validators.required, Validators.email]),
+      // selected: new FormControl(this.DataTeacher?.selected),
       //dischargeDate: new FormControl(this.DataTeacher?.dischargeDate),
       placeResidence: new FormControl(this.DataTeacher?.placeResidence),
-      listCourse: new FormControl(this.DataTeacher?.listCourse||[]),
-      listTraining: new FormControl(this.DataTeacher?.listTraining||[]),
-      listSpecialty: new FormControl(this.DataTeacher?.listSpecialty||[]),
-      listExperience: new FormControl(this.DataTeacher?.listExperience||[]),
-      listDocument: new FormControl(this.DataTeacher?.listDocument||[]),
+      listCourse: new FormControl(this.DataTeacher?.listCourse || []),
+      listTraining: new FormControl(this.DataTeacher?.listTraining || []),
+      listSpecialty: new FormControl(this.DataTeacher?.listSpecialty || []),
+      listExperience: new FormControl(this.DataTeacher?.listExperience || []),
+      listDocument: new FormControl(this.DataTeacher?.listDocument || []),
       process: new FormControl(1),
       createdBy: new FormControl(this.DataTeacher?.dischargeDate)
-     
+
     });
   }
-  
 
-  
+
+
   onFileSelected(event: any) {
     // const File1 = event.target.files[0];
     //  console.log(this.documentForm.getRawValue());
     // var formdata=new FormData();
-   
+
     // this.docForm.get('FileType')?.setValue("1");
     // formdata.append('FileDetails', this.docForm.get('FileDetails')?.value);
     // formdata.append('FileType', "1");
- 
-   
-  
-    
-//  this._teacherService.archivo(formdata).subscribe({
-//   next: () => {
-//    console.log("guardado");
-//   },
-//   error: () => {
-  
-//   }
-// })
-  }
-  
-  submit() {
- 
- 
- 
-  this.teacherForm?.get('listDocument')?.setValue(this.DataTeacher?.listDocument);
-  this.teacherForm?.get('listExperience')?.setValue(this.DataTeacher?.listExperience);
-  this.teacherForm?.get('listTraining')?.setValue(this.DataTeacher?.listTraining);
-  if(this.teacherForm.valid)
 
-  this._teacherService.addUpdateTeacher(this.teacherForm.value).subscribe({
-    next: () => {
-      Swal.fire({
-              title: "Escuela Judicial",
-              text: 'Guardado correctamente.',
-              icon: "success"
-          }); 
+
+
+
+    //  this._teacherService.archivo(formdata).subscribe({
+    //   next: () => {
+    //    console.log("guardado");
+    //   },
+    //   error: () => {
+
+    //   }
+    // })
+  }
+
+  submit() {
+    this.teacherForm?.get('listDocument')?.setValue(this.DataTeacher?.listDocument);
+    this.teacherForm?.get('listExperience')?.setValue(this.DataTeacher?.listExperience);
+    this.teacherForm?.get('listTraining')?.setValue(this.DataTeacher?.listTraining);
+    console.log('====================================');
+    console.log(this.teacherForm.valid);
+    console.log('====================================');
+    if (this.teacherForm.valid)
+
+      this._teacherService.addUpdateTeacher(this.teacherForm.value).subscribe({
+        next: () => {
+          Swal.fire({
+            title: "Escuela Judicial",
+            text: 'Guardado correctamente.',
+            icon: "success"
+          });
           this._nav.navigate(['/teacher/teacher-admission-external/']);
-    },
-    error: () => {
-      Swal.fire({
+        },
+        error: () => {
+          Swal.fire({
             title: "Escuela Judicial",
             text: 'Intente nuevamente.',
             icon: "warning"
           });
-    }
-   })
-   
+        }
+      })
+  }
 
-    // emppty stuff
+  removeTraining(row: Training) {
+    this.DataTraining = [];
+    this.DataTraining = this.DataTeacher.listTraining.filter(x => x.trainingId != row.trainingId)
+    this.DataTeacher.listTraining = [...this.DataTraining]
+
   }
-  
-  removeTraining(row: Training){
-    this.DataTraining=[];
-    this.DataTraining=this.DataTeacher.listTraining.filter(x=>x.trainingId!=row.trainingId)
-    this.DataTeacher.listTraining=[...this.DataTraining]
-    
-  }
-  
+
 
 }
 
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,15 +1,15 @@
 
 export const environment = {
   production: false,
-  base:'http://erp.organo-judicial.com/#/student/',
-  apiUrlTeacher:'http://localhost:5059/api/v1/Teacher/',
-  apiUrl:'https://isjupauthenticationservice.azurewebsites.net/api/v1/account/',
+  base: 'http://erp.organo-judicial.com/#/student/',
+  apiUrlTeacher: 'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/Teacher/',
+  apiUrl: 'https://isjupauthenticationservice.azurewebsites.net/api/v1/account/',
   apiUrlSchedule: "https://isjupeccurriculummanagementservice.azurewebsites.net/api/v1/",
-  ConsultaDocentes:'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/',
+  ConsultaDocentes: 'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/',
   consultaEstudiante: 'https://ecinscriptionservice-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/Cedula/DataqueryCedula/GetDataCedula/',
   apiEC: "https://ecinscriptionservice-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/",
-  apiUrlEF:'https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/Subject/',
+  apiUrlEF: 'https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/Subject/',
   apiEF: "https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/",
 };
-  
+
 
