3ba6f41b97a140e68adc73fd6ce950928bf2c8ac
Ricardo Cisnero <rcisnerocoronado@gmail.com>
2024-01-26 15:28:34 -0400

    url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   base:'http://erp.organo-judicial.com/#/student/',
-  apiUrlTeacher:'http://localhost:5059/api/v1/Teacher/',
+  apiUrlTeacher:'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/Teacher/',
   apiUrl:'https://isjupauthenticationservice.azurewebsites.net/api/v1/account/',
   apiUrlSchedule: "https://isjupeccurriculummanagementservice.azurewebsites.net/api/v1/",
   ConsultaDocentes:'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/',
