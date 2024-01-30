7f0fdb06cbc9aa8115d00e26a34a335339812154
Ricardo Cisnero <rcisnerocoronado@gmail.com>
2024-01-30 09:08:20 -0400

    validaciones

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -10,6 +10,7 @@ export const environment = {
   apiEC: "https://ecinscriptionservice-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/",
   apiUrlEF: 'https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/Subject/',
   apiEF: "https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/",
+  apiUrlDocument:'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/RequiredDocument/',
 
 };
 
