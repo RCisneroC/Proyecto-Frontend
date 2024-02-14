ffa55562d427f591de093c08888914da4528244a
JJIMENEZ <jjimenez@3tech-panama.com>
2024-02-14 17:09:04 -0500

    cambios

---
+++ src/app/enrollment/Encuestas/encuesta-activity/encuesta-activity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encuesta-activity',
+  templateUrl: './encuesta-activity.component.html',
+  styleUrls: ['./encuesta-activity.component.scss']
+})
+export class EncuestaActivityComponent {
+
+}
---
+++ src/app/enrollment/Encuestas/encuesta-subject/encuesta-subject.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encuesta-subject',
+  templateUrl: './encuesta-subject.component.html',
+  styleUrls: ['./encuesta-subject.component.scss']
+})
+export class EncuestaSubjectComponent {
+
+}
