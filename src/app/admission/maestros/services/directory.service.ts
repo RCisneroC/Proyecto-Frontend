4b8ff7eb92a9e6f6994e9ca05a9d07742a227fa9
JJIMENEZ <jjimenez@3tech-panama.com>
2024-02-26 14:53:16 -0500

    ricardo

--- src/app/admission/maestros/services/directory.service.ts
+++ src/app/admission/maestros/services/directory.service.ts
@@ -15,7 +15,7 @@ export class DirectoryService extends UnsubscribeOnDestroyAdapter {
   private readonly API_URL = 'assets/data/activities.json';
   isTblLoading = true;
   dataChange: BehaviorSubject<Directory[]> = new BehaviorSubject<
-  Directory[]
+    Directory[]
   >([]);
   // Temporarily stores data from dialogs
   dialogData!: Directory;
@@ -31,7 +31,7 @@ export class DirectoryService extends UnsubscribeOnDestroyAdapter {
   /** CRUD METHODS */
   getAllDirectory(): void {
     this.subs.sink = this.httpClient
-      .get<Directory[]>(environment.apiUrlSchedule+'ActivityMode/GetAll')
+      .get<Directory[]>(environment.apiUrlSchedule + 'ActivityMode/GetAll')
       .subscribe({
         next: (data) => {
           this.isTblLoading = false;
@@ -44,61 +44,61 @@ export class DirectoryService extends UnsubscribeOnDestroyAdapter {
       });
   }
   getAllDirectory2() {
-   return this.httpClient.get(environment.apiDocument+'Folder/GetFolder?FolderId=0')
-  }
-  
-  getfilebyId(fileId:number) {
-    return this.httpClient.get<Data>(environment.apiDocument+'Files/GetFile?FileId='+fileId)
-   }
-   
-   getFolderbyId(folderId:number) {
-    return this.httpClient.get<Data>(environment.apiDocument+'Folder/GetFolder?FolferId='+folderId)
-   }
-   
-   addFile(data:any) {
-    return this.httpClient.post(environment.apiDocument+'Files/AddFile',data)
-   }
-   
-   addFolder(data:any) {
-    return this.httpClient.post(environment.apiDocument+'Folder/AddFolder',data)
-   }
-   
-   UpdateFolder(data:any) {
-   
-    //UpdateFolder?FolderId=6&NewFolderName=newfoldername
-    return this.httpClient.put(environment.apiDocument+'Folder/UpdateFolder',data);
-   }
-   
-   UpdateFile(data:any) {
-   
-    //UpdateFolder?FolderId=6&NewFolderName=newfoldername
-    return this.httpClient.put(environment.apiDocument+'Files/UpdateFile',data);
-   }
-   
-   GetHistoryFile(id:number) {
-   
-    //UpdateFolder?FolderId=6&NewFolderName=newfoldername
-    return this.httpClient.get<DataD>(environment.apiDocument+'Folder/GetAuditFile?FileId='+id)
-  }
-   
-   
-   GetHistoryFolder(id:number) {
-   
-    return this.httpClient.get<DataD>(environment.apiDocument+'Folder/GetAuditFolder?FolderId='+id)
-   }
-   
-   
-   
-  
-  addDirectory(directory: Directory){
-    return this.httpClient.post<ResponseGenerica>(environment.apiUrlSchedule+'ActivityMode/Create', directory);
+    return this.httpClient.get(environment.apiDocument + 'Folder/GetFolder?FolderId=0')
   }
 
-  updateDirectory(directory: Directory){
+  getfilebyId(fileId: number) {
+    return this.httpClient.get<Data>(environment.apiDocument + 'Files/GetFile?FileId=' + fileId)
+  }
+
+  getFolderbyId(folderId: number) {
+    return this.httpClient.get<Data>(environment.apiDocument + 'Folder/GetFolder?FolferId=' + folderId)
+  }
+
+  addFile(data: any) {
+    return this.httpClient.post(environment.apiDocument + 'Files/AddFile', data)
+  }
+
+  addFolder(data: any) {
+    return this.httpClient.post(environment.apiDocument + 'Folder/AddFolder', data)
+  }
+
+  UpdateFolder(data: any) {
+
+    //UpdateFolder?FolderId=6&NewFolderName=newfoldername
+    return this.httpClient.put(environment.apiDocument + 'Folder/UpdateFolder', data);
+  }
+
+  UpdateFile(data: any) {
+
+    //UpdateFolder?FolderId=6&NewFolderName=newfoldername
+    return this.httpClient.put(environment.apiDocument + 'Files/UpdateFile', data);
+  }
+
+  GetHistoryFile(id: number) {
+
+    //UpdateFolder?FolderId=6&NewFolderName=newfoldername
+    return this.httpClient.get<DataD>(environment.apiDocument + 'Files/GetAuditFile?FileId=' + id)
+  }
+
+
+  GetHistoryFolder(id: number) {
+
+    return this.httpClient.get<DataD>(environment.apiDocument + 'Folder/GetAuditFolder?FolderId=' + id)
+  }
+
+
+
+
+  addDirectory(directory: Directory) {
+    return this.httpClient.post<ResponseGenerica>(environment.apiUrlSchedule + 'ActivityMode/Create', directory);
+  }
+
+  updateDirectory(directory: Directory) {
     return this.httpClient.put<ResponseGenerica>(environment.apiUrlSchedule + 'ActivityMode/Update', directory);
   }
 
-  DeleteActivityMode(Id: number){
+  DeleteActivityMode(Id: number) {
     const data = {
       id: Id
     };
@@ -108,10 +108,10 @@ export class DirectoryService extends UnsubscribeOnDestroyAdapter {
       }),
       body: data,
     };
-    
-  return this.httpClient.delete<ResponseGenerica>(environment.apiUrlSchedule+'ActivityMode/Delete',options);
+
+    return this.httpClient.delete<ResponseGenerica>(environment.apiUrlSchedule + 'ActivityMode/Delete', options);
   }
-  
+
 
 
 }
