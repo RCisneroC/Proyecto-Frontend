9a3d6911be7d1d665462dd34969d79a177258481
Ricardo Cisnero <rcisnerocoronado@gmail.com>
2024-02-26 14:20:31 -0400

    directorios

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   base: 'http://erp.organo-judicial.com/#/student/',
   baseUrlTeacher: 'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com',
-  apiUrlTeacher: 'http://localhost:5059/api/v1/Teacher/',
+  apiUrlTeacher: 'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/Teacher/',
   apiUrl: 'https://isjupauthenticationservice.azurewebsites.net/api/v1/Account/',
   apiUrlSchedule: "https://isjupeccurriculummanagementservice.azurewebsites.net/api/v1/",
   ConsultaDocentes: 'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/',
@@ -14,11 +14,11 @@ export const environment = {
   apiUrlEF: 'https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/Subject/',
   apiUrlEC: 'https://isjupeccurriculummanagementservice.azurewebsites.net/api/v1/Activity/',
   apiEF: "https://isjupefcurriculummanagementservice.azurewebsites.net/api/v1/",
-  apiUrlDocument: 'http://localhost:5059/api/v1/RequiredDocument/',
+  apiUrlDocument: 'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/RequiredDocument/',
   apiIntranet: 'https://eira-api.azurewebsites.net/',
   apiUrlSubjectTask: "https://eira-api.azurewebsites.net/SearchSubjectTask",
   apiEira: 'https://eira-api.azurewebsites.net/',
-  apiDocument:'http://localhost:5059/api/v1/'
+  apiDocument:'https://ecmanagementteaching-escuela-judicial.apps.revisados-attt.8ckj.p1.openshiftapps.com/api/v1/'
   
   
 };
