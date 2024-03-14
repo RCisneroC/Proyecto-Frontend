aa49eca7ec345a7d8378225d7cf00536b69011b4
Ibsen Perez <Ibsen.Perez@3tech-panama.com>
2024-03-13 19:09:53 -0500

    Estadisticas Docentes

--- src/app/estadisticas/estadisticas.module.ts
+++ src/app/estadisticas/estadisticas.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule,DatePipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ComponentsModule } from "@shared/components/components.module";
+import { SharedModule } from "@shared";
+import { VerificarBS64Pipe } from 'app/pipes/verificar-bs64.pipe';
+
+
 
 import { EstadisticasRoutingModule } from './estadisticas-routing.module';
 import { TablaPersonalDocenteComponent } from './PersonalDocente/tabla-personal-docente/tabla-personal-docente.component';
 
 
+
 @NgModule({
-  declarations: [
-    TablaPersonalDocenteComponent
-  ],
-  imports: [
-    CommonModule,
-    EstadisticasRoutingModule
-  ]
+   providers: [VerificarBS64Pipe, DatePipe],
+    declarations: [
+        TablaPersonalDocenteComponent
+    ],
+    imports: [
+        CommonModule,
+        EstadisticasRoutingModule,
+        ComponentsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule
+    ]
 })
 export class EstadisticasModule { }
