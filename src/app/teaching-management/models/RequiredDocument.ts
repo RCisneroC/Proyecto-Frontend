90fe3b44b1512ce2541005a53905e949891ec150
Roilan Armenteros <armenterosroilan@gmail.com>
2024-04-24 16:14:36 -0500

    up

--- src/app/teaching-management/models/RequiredDocument.ts
+++ src/app/teaching-management/models/RequiredDocument.ts
@@ -4,6 +4,7 @@
         name: string="";
         description: string="";
         point: number = 0;
+        isRecord: boolean = false;
         statusId: number=1;
         typeEducationId: number=0;
 
--- src/app/teaching-management/required-document-form/required-document-form.component.ts
+++ src/app/teaching-management/required-document-form/required-document-form.component.ts
@@ -59,6 +59,7 @@ export class RequiredDocumentFormComponent implements OnInit {
       name: [this.requiredDocument.name, [Validators.required]],
       description: [this.requiredDocument.description, [Validators.required]],
       point: [this.requiredDocument.point, [Validators.required]],
+      isRecord:[this.requiredDocument.isRecord, [Validators.required]],
       statusId: [this.requiredDocument.statusId, [Validators.required]],
       typeEducationId: [this.requiredDocument.typeEducationId.toString(), [Validators.required]],
       createdBy: [''],
@@ -72,7 +73,7 @@ export class RequiredDocumentFormComponent implements OnInit {
     this.dialogRef.close();
   }
   public confirmAdd(): void {
-
+      this.requiredDocumentForm.controls['isRecord'].setValue(this.requiredDocumentForm.getRawValue().isRecord == 'true')
       this.teacherService.updateRequiredDocument(this.requiredDocumentForm.getRawValue())
         .subscribe({
           next: () => {
