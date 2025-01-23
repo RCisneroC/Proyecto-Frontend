e2a2b89accdbef9d987760c7913b4c17bd9aa939
Ricardo Cisnero <rcisnerocoronado@gmail.com>
2025-01-23 17:29:05 -0400

    css contra

--- src/app/authentication/change-password/change-password.component.ts
+++ src/app/authentication/change-password/change-password.component.ts
@@ -6,11 +6,12 @@ import { AuthService } from '@core';
 import { User } from '@core/models/user';
 import { UnsubscribeOnDestroyAdapter } from '@shared';
 import { ResponseMessageMaestra } from 'app/admission/models/ResponseMessage';
-import { PasswordValidator } from 'app/CallsTeachers/models/PasswordValidator';
 
 import { UserService } from 'app/security/user/service/user.service';
 import { DialogData } from 'app/security/user/user-form/user-form.component';
 import Swal from 'sweetalert2';
+import * as CryptoJS from 'crypto-js';
+import { PasswordValidator } from 'app/CallsTeachers/models/PasswordValidator';
 
 @Component({
   selector: 'app-change-password',
@@ -29,6 +30,9 @@ implements OnInit {
   userForm!: UntypedFormGroup;
   user!: User;
   showPasswordCriteria=false;
+  hide1 = true;
+  hide2 = true;
+  hide3 = true;
   passwordValidations: any = {};
 constructor(
   public dialogRef: MatDialogRef<ChangePasswordComponent>,
@@ -60,6 +64,9 @@ constructor(
         isValidLength: value.length >= 8
       };
     });
+    
+    
+ 
   }
 
 
@@ -93,7 +100,7 @@ constructor(
       id:[this.user.id, [Validators.required]],
       currentPassword:["", [Validators.required]],
       newPassword: ['', [Validators.required,PasswordValidator.strongPasswordValidator()]],
-      confirmPassword:["", [Validators.required]],
+      confirmPassword:["", [Validators.required]]
     });
   }
   submit() {
@@ -102,35 +109,84 @@ constructor(
   onNoClick(): void {
     this.dialogRef.close();
   }
-  public confirmAdd(): void {
-   
-      this.userService.changePassword(this.userForm.getRawValue()).subscribe({
-        next: (res:any) => {
-          console.log(res);
-          if(res.message == 'Password actual incorrecto'){
-            this.ResponseMessage.CodError = 200;
-            this.ResponseMessage.Message = res.message; 
-            Swal.fire({
-              title: "Escuela Judicial",
-              text: 'Password actual incorrecto.',
-              icon: "warning"
-            });
-          }else{
-            this.ResponseMessage.CodError = 200;
-          this.ResponseMessage.Message = res.message;
-          this.dialogRef.close(this.ResponseMessage);
-          }
-          
-        },
-        error: (error: HttpErrorResponse) => { 
-          Swal.fire({
-            title: "Escuela Judicial",
-            text: error.error.Details,
-            icon: "warning"
-          }); 
-        },
+  
+ 
+
+// Ejemplo de cifrado de contraseña
+
+
+
+
+
+
+encryptPassword(password: string): string {
+  const secretKey = CryptoJS.enc.Utf8.parse('1234567890123456'); // Clave de 16 bytes
+  const iv = CryptoJS.enc.Utf8.parse('1234567890123456'); // IV de 16 bytes
+
+  const encrypted = CryptoJS.AES.encrypt(password, secretKey, {
+    iv: iv,
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+  });
+
+  return encrypted.toString();
+}
+
+
+
+
+
+public confirmAdd(): void {
+  // Guardar temporalmente las contraseñas originales
+  const originalNewPassword = this.userForm.get('newPassword')?.value;
+  const originalCurrentPassword = this.userForm.get('currentPassword')?.value;
+  const originalConfirmPassword = this.userForm.get('confirmPassword')?.value;
+
+  // Usar la función de cifrado
+  const encryptedNewPassword = this.encryptPassword(originalNewPassword);
+  const encryptedCurrentPassword = this.encryptPassword(originalCurrentPassword);
+  const encryptedConfirmPassword = this.encryptPassword(originalConfirmPassword);
+
+  // Parchar los valores cifrados en el formulario
+  this.userForm.patchValue({ newPassword: encryptedNewPassword });
+  this.userForm.patchValue({ currentPassword: encryptedCurrentPassword });
+  this.userForm.patchValue({ confirmPassword: encryptedConfirmPassword });
+
+  // Llamar al servicio con las contraseñas cifradas
+  this.userService.changePassword(this.userForm.getRawValue()).subscribe({
+    next: (res: any) => {
+      console.log(res);
+      if (res.message == 'Password actual incorrecto') {
+        this.ResponseMessage.CodError = 200;
+        this.ResponseMessage.Message = res.message;
+        Swal.fire({
+          title: "Escuela Judicial",
+          text: 'Password actual incorrecto.',
+          icon: "warning"
+        });
+      } else {
+        this.ResponseMessage.CodError = 200;
+        this.ResponseMessage.Message = res.message;
+        this.dialogRef.close(this.ResponseMessage);
+      }
+      // Restaurar las contraseñas originales en el formulario
+      this.userForm.patchValue({ newPassword: originalNewPassword });
+      this.userForm.patchValue({ currentPassword: originalCurrentPassword });
+      this.userForm.patchValue({ confirmPassword: originalConfirmPassword });
+    },
+    error: (error: HttpErrorResponse) => {
+      Swal.fire({
+        title: "Escuela Judicial",
+        text: error.error.Details,
+        icon: "warning"
       });
-      
-  }
+      // Restaurar las contraseñas originales en el formulario
+      this.userForm.patchValue({ newPassword: originalNewPassword });
+      this.userForm.patchValue({ currentPassword: originalCurrentPassword });
+      this.userForm.patchValue({ confirmPassword: originalConfirmPassword });
+    }
+  });
+}
+
 
 }
